01c7ed78276da1a6e9c4a04995199bb08dcf9d87
JuricLeo <ljurickc@gmail.com>
2024-05-28 11:22:49 +0200

    auth design fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,20 @@
+import Image from "next/image";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">{children}</div>
+    <div className="flex flex-col-reverse lg:flex-row h-screen">
+      <div className="flex flex-1 flex-col justify-between h-full px-6 py-8 bg-black">
+        <div className="flex items-center justify-center lg:justify-start">
+          <Image src="/logo-light.svg" alt="" width={100} height={100} />
+          <p className="ml-4 text-white text-2xl">FinancialFocus</p>
+        </div>
+        <p className="text-white text-center lg:text-start">
+          Automate your financial tracking and budgeting, reducing manual effort
+          and allowing you to focus on more important tasks.
+        </p>
+      </div>
+      <div className="flex-1 flex justify-center items-center">{children}</div>
+    </div>
   );
 };
 
